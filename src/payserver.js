ac8b28d63f72d9d3d6cc9913a4adbf6b26a49728
Matthew Bowyer <mattcbow@gmail.com>
2019-02-26 04:45:18 +0000

    Bow reciept fix

--- src/payserver.js
+++ src/payserver.js
@@ -2,6 +2,7 @@ import { WalletClient } from 'bclient';
 import { Network } from 'bcoin';
 import WebSocket from 'ws';
 import redis from 'redis';
+import { promisify } from 'util'
 
 import Client from './client';
 
@@ -9,9 +10,25 @@ class Payserver {
     constructor() {
         this.redisClientPlayers = this.connectToRedis(6379, 'redis-players')
         this.redisClientGames = this.connectToRedis(6379, 'redis-gameservers')
-        this.server = new WebSocket.Server({ port: 7000 });
         this.clients = {};
+        this.server = new WebSocket.Server({ port: 7000 });
         this.server.on('connection', this.newCustomer.bind(this));
+        if (process.env.NET == 'main' || process.env.NET == 'testnet') {
+            const network = Network.get(process.env.NET);
+            const walletOptions = {
+                port: network.walletPort,
+                host: process.env.BCOIN,
+                network: network.type,
+                apiKey: process.env.APIKEY,
+                ssl: (process.env.SSL === 'true')
+            };
+            this.walletClient = new WalletClient(walletOptions);
+            this.wallet = this.walletClient.wallet('primary');
+            (async () => {
+                const result = await this.wallet.getAccount('default');
+                this.defaultAccount = result;
+            })();
+        }
     }
 
     connectToRedis(port, name) {
